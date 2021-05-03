8aefd76f35c0cd989ca51fb5ea907c9b7e052713
Fabrice Beya <beyafabrice@gmail.com>
2021-05-03 04:00:10 +0200

    Initial Commit

---
+++ Smart Hommie WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smart Hommie WatchKit Extension
+//
+//  Created by Fabrice Beya on 2021/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Smart Hommie WatchKit Extension/Smart_HommieApp.swift
@@ -0,0 +1,19 @@
+//
+//  Smart_HommieApp.swift
+//  Smart Hommie WatchKit Extension
+//
+//  Created by Fabrice Beya on 2021/05/03.
+//
+
+import SwiftUI
+
+@main
+struct Smart_HommieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
